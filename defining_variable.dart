82bb0e187dd976f559b21f45cf9deef63ae42508
Ashwani1207 <57230745+Ashwani1207@users.noreply.github.com>
2020-07-05 03:48:24 +0530

    Create defining_variable.dart

---
+++ defining_variable.dart
@@ -0,0 +1,22 @@
+void main() {
+  int age=22;
+  var count=22;
+  int value=0XEADEBAEE;
+  double numb=2.4;
+  var new_num=2.8;
+  double n=2.8e10;
+  String name="Ashwani";
+  var dost="Aman";
+  bool isvalid=true;
+  var isntvalid=false;
+  print(age);
+  print(count);
+  print(value);
+  print(numb);
+  print(new_num);
+  print(name);
+  print(dost);
+  print(isvalid);
+  print(isntvalid);
+  print(n);
+}
