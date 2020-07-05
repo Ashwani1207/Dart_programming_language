41e35babb96da2712cb6298415829a100bf2ae53
Ashwani1207 <57230745+Ashwani1207@users.noreply.github.com>
2020-07-05 19:39:28 +0530

    Create constants_final_const.dart

---
+++ constants_final_const.dart
@@ -0,0 +1,18 @@
+void main() {
+  //Constants in dart
+  final name="Ashwani";
+  final String course="B.tech";
+  const pi=3.14;
+  const double r=8.314;
+  print(name);
+  print(course);
+  print(pi);
+  print(r);
+  /*class Grp
+  {
+    final br="cse";
+    //const pi=3.14'
+    
+  }
+  */
+}
