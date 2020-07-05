959bdd788c8d5fcb044c5691c6e22eb5f336da25
Ashwani1207 <57230745+Ashwani1207@users.noreply.github.com>
2020-07-05 20:14:40 +0530

    Create conditionalStatement_IfElseLadder.dart

---
+++ conditionalStatement_IfElseLadder.dart
@@ -0,0 +1,28 @@
+void main()
+{
+  var salary=25000;
+  //conditional statement
+  if (salary>20000)
+  {
+  print("you got promotion");
+  print("good");
+  }
+  else
+  print("you need to work hard");
+  //if_else_ladder
+  int marks=-8;
+  if(marks>=90&&marks<100)
+    print("A+");
+  else if(marks>=80&&marks<90)
+    print("A");
+  else if(marks>=70&&marks<80)
+    print("B+");
+  else if(marks>=60&&marks<70)
+    print("B");
+  else if(marks>=40&&marks<60)
+    print("you are passed");
+  else if(marks>=0&&marks<30)
+    print("You are failed");
+  else
+    print("Invalid input");
+}
