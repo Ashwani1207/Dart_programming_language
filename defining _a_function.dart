d9a062962353930508866964fc9bbeb2e6825f32
Ashwani1207 <57230745+Ashwani1207@users.noreply.github.com>
2020-07-06 19:51:02 +0530

    Update defining _a_function.dart

--- defining _a_function.dart
+++ defining _a_function.dart
@@ -1,9 +1,15 @@
 void main()
   {
    int a=area(3,4);
-  print("the perimetr of revtangle $a");
+  perimeter(10,20);
+  print("The area of rectangle  $a");
   }
 int area(int length,int breadth)
 {
  return length*breadth;
+}
+void perimeter(int length,int breadth)
+{
+  int p=2*(length+breadth);
+  print("The perimeter of rectangle length $length and breadth $breadth is $p");
 }
