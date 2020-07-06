31c7b7f116117f29f9da9d2b295c70897cc9ac86
Ashwani1207 <57230745+Ashwani1207@users.noreply.github.com>
2020-07-06 14:09:47 +0530

    Create Loops_for_while_dowhile.dart

---
+++ Loops_for_while_dowhile.dart
@@ -0,0 +1,23 @@
+
+void main()
+{
+  int i,j=6,k=4;
+  print("for loop");
+  for(i=0;i<5;i++)
+  {
+    print(i);
+  }
+  print("while loop");
+  while(j>0)
+  {
+    print(j);
+    j--;
+  }
+  print("do while loop");
+  do{
+    print(k);
+    k--;
+    
+  }while(k>0);
+}
+​
